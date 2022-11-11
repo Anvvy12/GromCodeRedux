60cef5f7c0a2af17b66554b923938d09b40aa9d0
Bogdan <youbody7@gmail.com>
2022-11-11 17:54:45 +0200

    s

--- lesson2/hw1/src/App.jsx
+++ lesson2/hw1/src/App.jsx
@@ -1,38 +1,7 @@
 import React, { Component } from "react";
-import ThemedButton from "./ThemedButton.jsx";
-import { themes, ThemeContext } from "./themes-context.js";
 
 export default class extends Component {
-  state = {
-    theme: themes.light,
-  };
-  toggle = () => {
-    const newTheme =
-      this.state.theme === themes.dark ? themes.light : themes.dark;
-    this.setState({
-      theme: newTheme,
-    });
-  };
   render() {
-    // return (
-    //   <div>
-    //     <ThemeContext.Provider value={this.state.theme}>
-    //       <ThemedButton onClick={this.toggle}>Dynamic Theme</ThemedButton>
-    //     </ThemeContext.Provider>
-    //     <ThemeContext.Provider>
-    //       <ThemedButton onClick={this.toggle}>Default Theme</ThemedButton>
-    //     </ThemeContext.Provider>
-    //   </div>
-    // );
-
-    return (
-      <div>
-        <ThemeContext.Provider value={this.state.theme}>
-          <ThemedButton onClick={this.toggle}>Dynamic Theme</ThemedButton>
-        </ThemeContext.Provider>
-
-        <ThemedButton onClick={this.toggle}>Default Theme</ThemedButton>
-      </div>
-    );
+    return null;
   }
 }
