5f0bdbe886f239cb192d70a679bc59414f76c3b8
Bogdan <youbody7@gmail.com>
2022-11-13 16:49:59 +0200

    s

--- lesson3/hw1/src/cart.reducer.js
+++ lesson3/hw1/src/cart.reducer.js
@@ -1,16 +1,16 @@
 import { ADD_PRODUCT, DELETE_PRODUCT } from "./cart.actions";
 
-const initialState = { products: [] };
+const initialState = { cart: [] };
 
 const productsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCT:
       return {
         ...state,
-        products: state.products.concat(action.payload.item),
+        products: state.cart.concat(action.payload.item),
       };
     case DELETE_PRODUCT:
-      const newList = state.products.filter(
+      const newList = state.cart.filter(
         (product) => product.id !== action.payload.id
       );
       return {
