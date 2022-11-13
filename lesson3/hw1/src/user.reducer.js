30d7a64743d6a891a8c22b3cea3674624a5ea505
Bogdan <youbody7@gmail.com>
2022-11-13 16:36:42 +0200

    s

--- lesson3/hw1/src/user.reducer.js
+++ lesson3/hw1/src/user.reducer.js
@@ -1,19 +1,14 @@
 import { ADD_USER, DELETE_USER } from "./user.actions";
 
-const initialState = { users: [] };
+const initialState = null;
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_USER:
-      return {
-        ...state,
-        users: state.users.concat(action.payload.user),
-      };
+      return action.payload.user;
+
     case DELETE_USER:
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.payload.id),
-      };
+      return null;
     default:
       return state;
   }
