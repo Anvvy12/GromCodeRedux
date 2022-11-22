498ce4d7c6ada734f5906470aeb64ea172103581
Bogdan <youbody7@gmail.com>
2022-11-22 13:12:06 +0200

    s

--- lesson5/hw1/src/App.jsx
+++ lesson5/hw1/src/App.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { Provider } from "react-redux";
 const App = () => {
-  return null;
+  return (
+    <div>
+      <div class="filter">
+        <span class="filter__count"></span>
+        <input type="text" class="filter__input" value="" />
+      </div>
+      <ul class="users">
+        <li class="user">
+          <span class="user__name">Tad</span>
+          <span class="user__age">18</span>
+        </li>
+
+        <li class="user">
+          <span class="user__name">Anna</span>
+          <span class="user__age">45</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
