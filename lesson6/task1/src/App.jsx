7a1fbec1115157ffe9b5d3e465e2ded17f96767d
Bogdan <youbody7@gmail.com>
2022-11-24 12:05:01 +0200

    s

--- lesson6/task1/src/App.jsx
+++ lesson6/task1/src/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default classNameNameName extends Component {
-  render() {
-    return null;
-  }
-}
+const App = () => {
+  return null;
+};
+
+export default App;
