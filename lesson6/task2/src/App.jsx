2cebccf0b07db739ccbfcd4547fbe03a2d42bf4c
Bogdan <youbody7@gmail.com>
2022-11-24 17:46:56 +0200

    s

--- lesson6/task2/src/App.jsx
+++ lesson6/task2/src/App.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import UserInfo from "./users/UserInfo";
+import SearchField from "./users/SearchField";
 import { Provider } from "react-redux";
+import Spinner from "./users/Spinner";
+
 import store from "./store";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <UserInfo />
+      <div className="page">
+        <UserInfo />
+        <SearchField />
+        <Spinner />
+      </div>
     </Provider>
   );
 };
